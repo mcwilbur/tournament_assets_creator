33cef6e30618bb0e32ef0f9395e23cb8683ffb67
McWilbur <jonathan.schnyder@gmail.com>
2022-03-29 21:11:43 +0200

    publish

--- js/about.e2004de2.js
+++ js/about.e2004de2.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunktournament_assets_creator"]=self["webpackChunktournament_assets_creator"]||[]).push([[443],{7381:function(t,n,a){a.r(n),a.d(n,{default:function(){return l}});var e=a(3396);const r={class:"about"},s=(0,e._)("h1",null,"This is an about page",-1),u=[s];function c(t,n){return(0,e.wg)(),(0,e.iD)("div",r,u)}var o=a(89);const i={},f=(0,o.Z)(i,[["render",c]]);var l=f}}]);
-//# sourceMappingURL=about.15d0fd52.js.map
+//# sourceMappingURL=about.e2004de2.js.map
